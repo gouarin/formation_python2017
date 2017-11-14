cb608477694efaaf23d883b2892f59b5b01e8428
Roland Denis <roland.denis@math.univ-lyon1.fr>
2017-11-14 09:41:43 +0100

    Fancy print of the perfs.

--- nbody/numba/nbody/barnes_hut_array/energy.py
+++ nbody/numba/nbody/barnes_hut_array/energy.py
@@ -1,28 +1,37 @@
+from __future__ import print_function
+
 import numpy as np
 from .quadTree import quadArray
 import time
 
+
 def compute_energy(mass, particles, energy):
+    print('compute energy:')
+    t_tot = time.time()
+
     bmin = np.min(particles[: ,:2], axis=0)
     bmax = np.max(particles[: ,:2], axis=0)
     root = quadArray(bmin, bmax, particles.shape[0])
 
-    print('build tree')
+    print('\tbuild tree:    ', end='', flush=True)
     t1 = time.time()
     root.buildTree(particles)
     t2 = time.time()
-    print(t2-t1)
+    print('{:9.4f}ms'.format(1000*(t2-t1)))
 
-    print('compute mass')
+    print('\tcompute mass:  ', end='', flush=True)
     t1 = time.time()
     root.computeMassDistribution(particles, mass)
     t2 = time.time()
-    print(t2-t1)
+    print('{:9.4f}ms'.format(1000*(t2-t1)))
 
+    print('\tcompute force: ', end='', flush=True)
     t1 = time.time()    
     for i in range(particles.shape[0]):
         acc = root.computeForce(particles[i])
         energy[i, 2:] = acc
     energy[:, :2] = particles[:, 2:]
     t2 = time.time()
-    print(t2-t1)
+    print('{:9.4f}ms'.format(1000*(t2-t1)))
+
+    print('\ttotal:       {:11.4f}ms'.format(1000*(time.time()-t_tot)))
