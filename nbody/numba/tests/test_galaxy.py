4c22b1c66481631ccd21cb164420cdc47ab119fe
Roland Denis <roland.denis@math.univ-lyon1.fr>
2017-11-15 11:31:34 +0100

    Testing color generation for stars (using alpha).

--- nbody/numba/tests/test_galaxy.py
+++ nbody/numba/tests/test_galaxy.py
@@ -1,10 +1,19 @@
 import numpy as np
 import sys
+import math
 
 import nbody
 #from nbody.naive import compute_energy
 from nbody.barnes_hut_array import compute_energy
 
+def temp2color(temps):
+    colors = np.empty( (temps.size, 4) )
+    colors[:, 0] = 1. - (temps-6400) * 1e-3*0.07
+    colors[:, 1] = 0.98 - np.abs(np.log(temps/6600)) * (0.25/math.log(2))
+    colors[:, 2] = 1. + (temps-6600) * 1e-3*0.2
+    colors[:, 3] = 1.
+    colors = np.minimum(1., np.maximum(0., colors))
+    return colors
 
 class Galaxy:
     def __init__(self, blackHole, dt = 1., display_step = 1):
@@ -21,6 +30,13 @@ class Galaxy:
 
     def coords(self):
         return self.particles[:, :2]
+
+    def colors(self):
+        speed_magnitude = np.linalg.norm(self.particles[:, 2:4], axis=1)
+        colors = temp2color( 3000 + 6000*speed_magnitude/speed_magnitude.max() )
+        colors[:,3] = 0.2
+        return colors + np.asarray([0., 0., 0., 0.8]) * np.minimum(self.mass, 20).reshape(-1, 1) / 20
+
 
 if __name__ == '__main__':
     import argparse
@@ -39,8 +55,13 @@ if __name__ == '__main__':
                 {'coord': [0, 0], 'mass': 1000000, 'svel': 1, 'stars': 2000, 'radstars': 3},
                 {'coord': [3, 3], 'mass': 1000000, 'svel': 0.9, 'stars': 1000, 'radstars': 1}
                 ]
-
     sim = Galaxy(blackHole, display_step = args.display_step)
-    anim = Animation( sim, axis=[-10, 10, -10, 10] )
+
+    print( temp2color( np.asarray([3000]) ) )
+
+    if args.render == 'opengl':
+        anim = Animation( sim, axis=[-10, 10, -10, 10], use_colors=True, update_colors=False )
+    else:
+        anim = Animation( sim, axis=[-10, 10, -10, 10] )
     anim.main_loop()
 
