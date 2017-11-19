c394e9ebbf9de50d36655c839d1e6634bb2bdf3e
Roland Denis <roland.denis@math.univ-lyon1.fr>
2017-11-19 15:34:55 +0100

    Better formula for black body temp -> RGB convertion

--- nbody/numba/tests/test_galaxy.py
+++ nbody/numba/tests/test_galaxy.py
@@ -7,13 +7,36 @@ import nbody
 from nbody.barnes_hut_array import compute_energy
 
 def temp2color(temps):
-    colors = np.empty( (temps.size, 4) )
-    colors[:, 0] = 1. - (temps-6400) * 1e-3*0.07
-    colors[:, 1] = 0.98 - np.abs(np.log(temps/6600)) * (0.25/math.log(2))
-    colors[:, 2] = 1. + (temps-6600) * 1e-3*0.2
-    colors[:, 3] = 1.
-    colors = np.minimum(1., np.maximum(0., colors))
+    """ Convert black body temperature to corresponding RGBA color.
+
+    See http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+    """
+    colors = np.empty((temps.size, 4))
+    scaled_temps = temps/100.
+
+    # Red component
+    ind = scaled_temps <= 66.
+    colors[ind, 0] = 255.
+    colors[~ind, 0] = 329.698727446 * ((scaled_temps[~ind] - 60.) ** -0.1332047592)
+
+    # Green component
+    colors[ind, 1] = 99.4708025861 * np.log(scaled_temps[ind]) - 161.1195681661
+    colors[~ind, 1] = 288.1221695283 * ((scaled_temps[~ind] - 60.) ** -0.0755148492)
+
+    # Blue component
+    ind = scaled_temps >= 66.
+    colors[ind, 2] = 255.
+    colors[~ind, 2] = 138.5177312231 * np.log(scaled_temps[~ind] - 10.) - 305.0447927307
+    colors[scaled_temps <= 19., 2] = 0.
+
+    # Alpha component
+    colors[:, 3] = 255.
+
+    # Clipping values
+    np.clip(colors/255., 0., 1., out=colors)
+
     return colors
+
 
 class Galaxy:
     def __init__(self, blackHole, dt = 1., display_step = 1):
