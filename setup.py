f69a6f56544bd21261952c5947b74b61b99f68ca
Ruben Di Battista <ruben.di-battista@polytechnique.edu>
2018-11-28 17:55:23 +0100

    [WIP] - Fixing deps

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
-from Cython.Build import cythonize
 
 setup(
     name="pyGalaxy",
-    #ext_modules=cythonize('pygalaxy/quadtree.pyx'),  # accepts a glob pattern
     packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'matplotlib',
+        'docopt',
+        'numba',
+
+    ],
 )
